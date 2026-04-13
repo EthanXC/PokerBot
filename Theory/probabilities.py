57408c7cba7b944ad75ae116f15f06730107b39d
Ethan <ethancartybb@gmail.com>
2026-04-13 17:22:50 -0400

    added class structure to probabilities

--- Theory/probabilities.py
+++ Theory/probabilities.py
@@ -2,6 +2,7 @@
 
 
 import random
+from dataclasses import dataclass
 
 '''
 I want to design this to be object oriented.
@@ -19,6 +20,32 @@ while True:
         break
 
     print ("Please enter an integer between 2 and 10")
-        
+
+VALID_RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+VALID_SUITS = ['H', 'D', 'C', 'S']
+
+# @dataclass is a decorator that creates a class with __init__ and __repr__ methods. Just easier to use.
+@dataclass(frozen=True)
+
+class Card:
+    rank: str
+    suit: str
+    '''
+    A card in a deck of 52 cards.
+    '''
+    pass
 
 
+
+class Deck:
+    pass
+
+class HandEvaluator:
+    pass
+
+class EquitySimulator:
+    '''
+    Calculates the equity of a hand against a range of hands. Or, a simulated probability of winning when you
+    cannot know the other hands.
+    '''
+    pass
